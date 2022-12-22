7bd05cb0dccf1c9b445402abb9618df35957fae3
JeffersonCabello <jefcsanc@espol.edu.ec>
2022-12-22 12:41:38 -0500

    Sensor GPS

--- app/src/main/java/com/example/g2_sensoresembebidos/MainActivity.java
+++ app/src/main/java/com/example/g2_sensoresembebidos/MainActivity.java
@@ -1,26 +1,52 @@
 package com.example.g2_sensoresembebidos;
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.PackageManagerCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Looper;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int REQUEST_CODE = 1;
     private Button camara;
-    private Button bluetooth;
+    private Button gps;
+    private Button salida;
+    EditText lati;
+    EditText longi;
+    FusedLocationProviderClient fusedLocationProviderClient;
+    ProgressBar progressBar;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        camara = (Button) findViewById(R.id.button_camara);
-        bluetooth = (Button) findViewById(R.id.button_bluetooth);
-
+        camara = (Button) findViewById(R.id.btnCamara);
+        gps = (Button) findViewById(R.id.btnObtenerCoordenda);
+        salida = findViewById(R.id.btnSalir);
+        lati = findViewById(R.id.edtlatitud);
+        longi = findViewById(R.id.edtlongitud);
 
         camara.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,5 +58,56 @@ public class MainActivity extends AppCompatActivity {
     private void onCamera(){
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(intent, 2);
+    }
+    public void ObtenerCoordenadas(View view) {
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_CODE);
+        } else {
+
+            getCoordenada();
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE && grantResults.length > 0) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getCoordenada();
+            } else {
+                Toast.makeText(this, "Permiso Denegado ..", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    private void getCoordenada() {
+        try {
+            LocationRequest locationRequest = new LocationRequest();
+            locationRequest.setInterval(10000);
+            locationRequest.setFastestInterval(3000);
+            locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                return;
+            }
+            LocationServices.getFusedLocationProviderClient(this).requestLocationUpdates(locationRequest, new LocationCallback() {
+                @Override
+                public void onLocationResult(LocationResult locationResult) {
+                    super.onLocationResult(locationResult);
+                    LocationServices.getFusedLocationProviderClient(MainActivity.this).removeLocationUpdates(this);
+                    if (locationResult != null && locationResult.getLocations().size() > 0) {
+                        int latestLocationIndex = locationResult.getLocations().size() - 1;
+                        double latitud = locationResult.getLocations().get(latestLocationIndex).getLatitude();
+                        double longitud = locationResult.getLocations().get(latestLocationIndex).getLongitude();
+                        longi.setText(String.valueOf(longitud));
+                        lati.setText(String.valueOf(latitud));
+                    }
+                }
+
+            }, Looper.myLooper());
+
+        }catch (Exception ex){
+            System.out.println("Error es :" + ex);
+        }
+
     }
 }
